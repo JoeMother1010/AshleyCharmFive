05bcdb8ff4c4a3024ec3c5f267e0c3417e10b83e
JoeMother1010 <joemother1010@outlook.com>
2025-01-16 01:35:43 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
   function copyWallet() {
-    const walletAddress = "0x1234567890abcdef"; // Replace with actual wallet address
+    const walletAddress = "3oUV7hiSriNJ3UxmoBs11ZQRZYUor8fXefokXfu7pump"; // Replace with actual wallet address
     navigator.clipboard.writeText(walletAddress).then(() => {
       alert("Wallet address copied to clipboard!");
     });
